3e50a21af458163fb411b8f7b1c3b75aaabcac12
RETITA <retitaferaudcharlys@yahoo.fr>
2018-07-20 19:57:15 +0200

    Actions utilisateur

--- app/src/main/java/com/example/retita/topquiz/MainActivity.java
+++ app/src/main/java/com/example/retita/topquiz/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.retita.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.edit_tewt_id);
         button = (Button)findViewById(R.id.button_id);
 
-        
+        //désactiver le boutton
+        button.setEnabled(false);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                editText.setEnabled(charSequence.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
