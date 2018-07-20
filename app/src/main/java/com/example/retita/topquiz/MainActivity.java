d3b650f82c8c560d1fb95eaa7659a19c99a9087f
RETITA <retitaferaudcharlys@yahoo.fr>
2018-07-20 19:46:16 +0200

    Référencement des éléments graphiques

--- app/src/main/java/com/example/retita/topquiz/MainActivity.java
+++ app/src/main/java/com/example/retita/topquiz/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.retita.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.text_view_id);
+        editText = (EditText)findViewById(R.id.edit_tewt_id);
+        button = (Button)findViewById(R.id.button_id);
+
+        
     }
 }
